3dd88b0693c5c1430b980af78e79a7a846176c18
hidny <mtardibuono@gmail.com>
2022-03-03 18:31:30 -0500

    Copied SeatedRightOfOpponentMellow from feature branch

--- src/mellow/ai/situationHandlers/SeatedRightOfOpponentMellow.java
+++ src/mellow/ai/situationHandlers/SeatedRightOfOpponentMellow.java
@@ -424,10 +424,13 @@ public class SeatedRightOfOpponentMellow {
 							&& dataModel.signalHandler.getMaxCardRankSignal(Constants.CURRENT_PARTNER_INDEX, leadSuit)
 									<= DataModel.getRankIndex(leaderThrow)
 							&& dataModel.getNumCardsHiddenInOtherPlayersHandsForSuit(leadSuit) >= 2
-							&& 
-							dataModel.getNumCardsInPlayOverCardSameSuit(
-								dataModel.signalHandler.getMaxRankCardMellowPlayerCouldHaveBasedOnSignals(MELLOW_PLAYER_INDEX, Constants.SPADE)
-							) > 5
+							&& (
+									dataModel.signalHandler.getMaxRankCardMellowPlayerCouldHaveBasedOnSignals(MELLOW_PLAYER_INDEX, Constants.SPADE)
+									== null
+								|| dataModel.getNumCardsInPlayOverCardSameSuit(
+									dataModel.signalHandler.getMaxRankCardMellowPlayerCouldHaveBasedOnSignals(MELLOW_PLAYER_INDEX, Constants.SPADE)
+								) > 5
+							)
 									
 								) {
 						//Fixes a debug case:
