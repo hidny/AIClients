ff150f5de0303495c8bc059907747c7bd11eb398
hidny <mtardibuono@gmail.com>
2022-10-25 07:50:59 -0400

    Added more logic to the SecretlyBurnt situation

--- src/mellow/ai/situationHandlers/wildSituations/SecretlyBurnt.java
+++ src/mellow/ai/situationHandlers/wildSituations/SecretlyBurnt.java
@@ -3,7 +3,9 @@ package mellow.ai.situationHandlers.wildSituations;
 import mellow.Constants;
 import mellow.ai.cardDataModels.DataModel;
 import mellow.ai.situationHandlers.NoMellowPlaySituation;
+import mellow.ai.situationHandlers.PartnerSaidMellowSituation;
 import mellow.ai.situationHandlers.SeatedLeftOfOpponentMellow;
+import mellow.ai.situationHandlers.SeatedRightOfOpponentMellow;
 import mellow.cardUtils.CardStringFunctions;
 import mellow.ai.cardDataModels.handIndicators.NonMellowBidHandIndicators;
 
@@ -14,6 +16,24 @@ public class SecretlyBurnt {
 		System.out.println("(Secretly Burned)");
 		
 		int throwIndex = dataModel.getCardsPlayedThisRound() % Constants.NUM_PLAYERS;
+		
+		//Extremely exceptional circumstances where there's another mellow:
+		if(dataModel.saidMellow(Constants.LEFT_PLAYER_INDEX)
+				&& dataModel.getNumTricks(Constants.LEFT_PLAYER_INDEX) == 0) {
+			System.out.println("(TRY TO BURN MELLOW ON RIGHT)");
+			return SeatedRightOfOpponentMellow.playMoveSeatedRightOfOpponentMellow(dataModel);
+		
+		} else if(dataModel.saidMellow(Constants.RIGHT_PLAYER_INDEX)
+				&& dataModel.getNumTricks(Constants.RIGHT_PLAYER_INDEX) == 0) {
+			System.out.println("(TRY TO BURN MELLOW ON LEFT)");
+			
+			return SeatedLeftOfOpponentMellow.playMoveSeatedLeftOfOpponentMellow(dataModel);
+		} else if(dataModel.saidMellow(Constants.CURRENT_PARTNER_INDEX)
+				&& dataModel.getNumTricks(Constants.RIGHT_PLAYER_INDEX) == 0) {
+			System.out.println("(TRY TO SAVE PARTNER MELLOW)");
+			return PartnerSaidMellowSituation.playMoveToProtectPartnerMellow(dataModel);
+		}
+		//End extremely exceptional circumstances
 		
 		if(dataModel.getIndexOfCurrentlyWinningPlayerBeforeAIPlays() == Constants.CURRENT_PARTNER_INDEX) {
 			return null;
